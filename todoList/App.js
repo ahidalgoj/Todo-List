39f196e26acda8b07a07f01206b285d3fb7c1c8a
Andres Hidalgo <ahidalgoj@ucenfotec.ac.cr>
2022-06-04 15:19:21 -0600

    Nuevos estilos definidos

--- todoList/App.js
+++ todoList/App.js
@@ -4,8 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Diana y Viviana!</Text>
-      <StatusBar style="auto" />
+
+      {/* Tareas de hoy*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tareas de Hoy</Text>
+
+        <View style={styles.items}>
+
+          {/* Aquí van las tareas*/}
+
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight: 'bold'
+  },
+  items:{},
 });
